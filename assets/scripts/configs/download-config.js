f9106de80f54a250b99cbe4c9304aad095fa7d8e
Re0XIAOPA <Re0XIAOPA@users.noreply.github.com>
2025-05-20 03:14:50 +0000

    Update site content 35287223e8484633a6bba99f891e3d59bd51cf29

--- assets/scripts/configs/download-config.js
+++ assets/scripts/configs/download-config.js
@@ -1,5 +1,5 @@
 // 下载链接配置  全是小写
-// 最后更新时间: 5/19/2025, 3:20:11 AM
+// 最后更新时间: 5/20/2025, 3:14:49 AM
 
 const downloadLinks = {
     "v2box": {
@@ -35,10 +35,10 @@ const downloadLinks = {
         "github": "https://github.com/2dust/v2rayN"
     },
     "clash verge": {
-        "version": "valpha",
-        "windows": "https://github.com/clash-verge-rev/clash-verge-rev/releases/download/alpha/Clash.Verge_2.2.4-alpha_x64-setup.exe",
-        "mac": "https://github.com/clash-verge-rev/clash-verge-rev/releases/download/alpha/Clash.Verge_2.2.4-alpha_x64.dmg",
-        "linux": "https://github.com/clash-verge-rev/clash-verge-rev/releases/download/alpha/Clash.Verge_2.2.4-alpha_amd64.deb",
+        "version": "v2.2.3",
+        "windows": "https://github.com/clash-verge-rev/clash-verge-rev/releases/download/v2.2.3/Clash.Verge_2.2.3_x64-setup.exe",
+        "mac": "https://github.com/clash-verge-rev/clash-verge-rev/releases/download/v2.2.3/Clash.Verge_2.2.3_x64.dmg",
+        "linux": "https://github.com/clash-verge-rev/clash-verge-rev/releases/download/v2.2.3/Clash.Verge_2.2.3_amd64.deb",
         "github": "https://github.com/clash-verge-rev/clash-verge-rev"
     },
     "mihomo party": {
