01348c5b57a780d21b05ee7ffa0d843865ff4b80
Re0XIAOPA <Re0XIAOPA@users.noreply.github.com>
2025-06-16 03:24:08 +0000

    Update site content 3078fadf098212a1b012f6cb1e167e97d40720db

--- assets/scripts/configs/download-config.js
+++ assets/scripts/configs/download-config.js
@@ -1,5 +1,5 @@
 // 下载链接配置  全是小写
-// 最后更新时间: 6/15/2025, 3:25:42 AM
+// 最后更新时间: 6/16/2025, 3:24:06 AM
 
 const downloadLinks = {
     "v2box": {
@@ -78,11 +78,11 @@ const downloadLinks = {
         "github": "https://github.com/GUI-for-Cores/GUI.for.Clash"
     },
     "flclash": {
-        "version": "v0.8.85",
-        "windows": "https://github.com/chen08209/FlClash/releases/download/v0.8.85/FlClash-0.8.85-windows-amd64-setup.exe",
-        "mac": "https://github.com/chen08209/FlClash/releases/download/v0.8.85/FlClash-0.8.85-macos-arm64.dmg",
-        "linux": "https://github.com/chen08209/FlClash/releases/download/v0.8.85/FlClash-0.8.85-linux-amd64.deb",
-        "android": "https://github.com/chen08209/FlClash/releases/download/v0.8.85/FlClash-0.8.85-android-arm64-v8a.apk",
+        "version": "v0.8.86",
+        "windows": "https://github.com/chen08209/FlClash/releases/download/v0.8.86/FlClash-0.8.86-windows-amd64-setup.exe",
+        "mac": "https://github.com/chen08209/FlClash/releases/download/v0.8.86/FlClash-0.8.86-macos-arm64.dmg",
+        "linux": "https://github.com/chen08209/FlClash/releases/download/v0.8.86/FlClash-0.8.86-linux-amd64.deb",
+        "android": "https://github.com/chen08209/FlClash/releases/download/v0.8.86/FlClash-0.8.86-android-arm64-v8a.apk",
         "github": "https://github.com/chen08209/FlClash"
     },
     "v2rayu": {
