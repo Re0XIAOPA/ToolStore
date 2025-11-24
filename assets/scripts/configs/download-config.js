ca49ffd9d76920fae5c43456282dd3703b1ebe4e
Re0XIAOPA <Re0XIAOPA@users.noreply.github.com>
2025-11-24 03:36:27 +0000

    🌐 部署网站更新 | 📦 更新内容和资源文件 27d91d9b91fc58b5ce860f7ad3326d46d66a6a5f

--- assets/scripts/configs/download-config.js
+++ assets/scripts/configs/download-config.js
@@ -1,5 +1,5 @@
 // 下载链接配置  全是小写
-// 最后更新时间: 11/23/2025, 3:38:59 AM
+// 最后更新时间: 11/24/2025, 3:36:26 AM
 
 const downloadLinks = {
     "v2box": {
@@ -59,8 +59,8 @@ const downloadLinks = {
         "github": "https://github.com/getsurfboard/surfboard"
     },
     "v2rayng": {
-        "version": "v1.10.26",
-        "android": "https://github.com/2dust/v2rayNG/releases/download/1.10.26/v2rayNG_1.10.26_universal.apk",
+        "version": "v1.10.28",
+        "android": "https://github.com/2dust/v2rayNG/releases/download/1.10.28/v2rayNG_1.10.28_universal.apk",
         "github": "https://github.com/2dust/v2rayNG"
     },
     "singbox": {
