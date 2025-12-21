bb234fd5682bc31951bbd5d0229288791d0ce799
Re0XIAOPA <mckekejun@outlook.com>
2025-12-21 18:54:14 +0800

    Update blog posts, tags, and sidebar; add new templates
    Removed outdated blog posts and images, added new blog templates for MD and MDX formats, and updated tags.yml with bilingual labels and new categories. Enhanced docs index with an in-depth article, adjusted Docusaurus config, added a Chinese sidebar translation, updated custom CSS for sidebar title localization, and included 'FlClash' in the recommend-config.js.

---
+++ docs/blog/2025-12-20-blog-template.md
@@ -0,0 +1,14 @@
+---
+slug: blog-template-md
+title: MD 模板
+authors: [Re0XIAOPA]
+tags: [日常]
+---
+
+这里写文章简介...
+
+<!-- truncate -->
+
+## 标题
+
+内容...
--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -60,6 +60,7 @@ const config: Config = {
           onInlineTags: 'warn',
           onInlineAuthors: 'warn',
           onUntruncatedBlogPosts: 'warn',
+         
         },
         theme: {
           customCss: './src/css/custom.css',
