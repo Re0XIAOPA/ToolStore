218468ba51a545785cc11904251358b696768245
Re0XIAOPA <Re0XIAOPA@users.noreply.github.com>
2025-12-26 03:37:12 +0000

    🌐 部署网站更新 | 📦 更新内容和资源文件 a31b18dc8becc6b465d07c6d9139167e5e31ed2d

--- assets/scripts/configs/download-config.js
+++ assets/scripts/configs/download-config.js
@@ -1,5 +1,5 @@
 // 下载链接配置  全是小写
-// 最后更新时间: 12/25/2025, 3:39:18 AM
+// 最后更新时间: 12/26/2025, 3:37:10 AM
 
 const downloadLinks = {
     "v2box": {
@@ -64,9 +64,9 @@ const downloadLinks = {
         "github": "https://github.com/2dust/v2rayNG"
     },
     "singbox": {
-        "version": "v1.12.13",
-        "windows": "https://github.com/SagerNet/sing-box/releases/download/v1.12.13/sing-box-1.12.13-windows-amd64-legacy-windows-7.zip",
-        "linux": "https://github.com/SagerNet/sing-box/releases/download/v1.12.13/sing-box_1.12.13_linux_amd64.deb",
+        "version": "v1.12.14",
+        "windows": "https://github.com/SagerNet/sing-box/releases/download/v1.12.14/sing-box-1.12.14-windows-amd64-legacy-windows-7.zip",
+        "linux": "https://github.com/SagerNet/sing-box/releases/download/v1.12.14/sing-box_1.12.14_linux_amd64.deb",
         "github": "https://github.com/SagerNet/sing-box",
         "ios": "https://apps.apple.com/us/app/sing-box-vt/id6673731168"
     },
