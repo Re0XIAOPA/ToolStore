96d3bb5e63e81128df28eb51f2ccc5b416ca1303
Re0XIAOPA <Re0XIAOPA@users.noreply.github.com>
2025-12-29 03:52:43 +0000

    🌐 部署网站更新 | 📦 更新内容和资源文件 4d8a4b02daafdc0ecde1b2fb817c6ec4d8b330db

--- assets/scripts/configs/download-config.js
+++ assets/scripts/configs/download-config.js
@@ -1,5 +1,5 @@
 // 下载链接配置  全是小写
-// 最后更新时间: 12/28/2025, 3:50:34 AM
+// 最后更新时间: 12/29/2025, 3:52:40 AM
 
 const downloadLinks = {
     "v2box": {
@@ -28,10 +28,10 @@ const downloadLinks = {
         "github": "https://github.com/MatsuriDayo/NekoBoxForAndroid"
     },
     "v2rayn": {
-        "version": "v7.16.8",
-        "windows": "https://github.com/2dust/v2rayN/releases/download/7.16.8/v2rayN-windows-64-desktop.zip",
-        "mac": "https://github.com/2dust/v2rayN/releases/download/7.16.8/v2rayN-macos-arm64.dmg",
-        "linux": "https://github.com/2dust/v2rayN/releases/download/7.16.8/v2rayN-linux-arm64.deb",
+        "version": "v7.16.9",
+        "windows": "https://github.com/2dust/v2rayN/releases/download/7.16.9/v2rayN-windows-64-desktop.zip",
+        "mac": "https://github.com/2dust/v2rayN/releases/download/7.16.9/v2rayN-macos-arm64.dmg",
+        "linux": "https://github.com/2dust/v2rayN/releases/download/7.16.9/v2rayN-linux-arm64.deb",
         "github": "https://github.com/2dust/v2rayN"
     },
     "clash verge": {
