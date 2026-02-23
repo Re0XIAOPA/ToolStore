3d923438bec1d5cc87aa99413c8673ad85ee9df1
Re0XIAOPA <Re0XIAOPA@users.noreply.github.com>
2026-02-23 04:28:06 +0000

    🌐 部署网站更新 | 📦 更新内容和资源文件 5107d03ca720bb00a4679103f1495cb95d8acbee

--- assets/scripts/configs/download-config.js
+++ assets/scripts/configs/download-config.js
@@ -1,9 +1,9 @@
 // 下载链接配置  全是小写
-// 最后更新时间: 2/22/2026, 4:22:01 AM
+// 最后更新时间: 2/23/2026, 4:28:04 AM
 
 const downloadLinks = {
     "v2box": {
-        "version": "v9.8.7",
+        "version": "v9.8.8",
         "android": "https://play.google.com/store/apps/details?id=dev.hexasoftware.v2box",
         "ios": "https://apps.apple.com/us/app/v2box-v2ray-client/id6446814690",
         "iosPrice": "Free"
