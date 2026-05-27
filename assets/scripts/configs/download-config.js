a678a49ad3216f75577995030d7c8a6ae61ab0ba
Re0XIAOPA <Re0XIAOPA@users.noreply.github.com>
2026-05-27 06:25:49 +0000

    🌐 部署网站更新 | 📦 更新内容和资源文件 95d2a8f97b7702e4b47eb6aa0ef2ac9b0b9c8651

--- assets/scripts/configs/download-config.js
+++ assets/scripts/configs/download-config.js
@@ -1,5 +1,5 @@
 // 下载链接配置  全是小写
-// 最后更新时间: 5/26/2026, 6:11:32 AM
+// 最后更新时间: 5/27/2026, 6:25:47 AM
 
 const downloadLinks = {
     "v2box": {
@@ -63,8 +63,8 @@ const downloadLinks = {
         "github": "https://github.com/MetaCubeX/ClashMetaForAndroid"
     },
     "surfboard": {
-        "version": "v1.1.0",
-        "android": "https://github.com/getsurfboard/surfboard/releases/download/tv-1.1.0/tv-universal-release.apk",
+        "version": "v2.31.0",
+        "android": "https://github.com/getsurfboard/surfboard/releases/download/mobile-2.31.0/mobile-universal-release.apk",
         "github": "https://github.com/getsurfboard/surfboard"
     },
     "v2rayng": {
