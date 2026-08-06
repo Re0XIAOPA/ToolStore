a70c891519adc1ec95bd3734f81d191c05c49324
Re0XIAOPA <Re0XIAOPA@users.noreply.github.com>
2026-08-06 05:21:14 +0000

    🌐 部署网站更新 | 📦 更新内容和资源文件 052d367160283c949032523d169d53d927f24131

--- assets/scripts/configs/download-config.js
+++ assets/scripts/configs/download-config.js
@@ -1,5 +1,5 @@
 // 下载链接配置  全是小写
-// 最后更新时间: 8/5/2026, 5:19:02 AM
+// 最后更新时间: 8/6/2026, 5:21:11 AM
 
 const downloadLinks = {
     "v2box": {
@@ -112,11 +112,11 @@ const downloadLinks = {
         "iosPrice": "Free"
     },
     "clashmi": {
-        "version": "v1.0.27",
-        "windows": "https://github.com/KaringX/clashmi/releases/download/v1.0.27.1301/clashmi_1.0.27.1301_windows_x64.exe",
-        "mac": "https://github.com/KaringX/clashmi/releases/download/v1.0.27.1301/clashmi_1.0.27.1301_macos_universal.dmg",
-        "linux": "https://github.com/KaringX/clashmi/releases/download/v1.0.27.1301/clashmi_1.0.27.1301_linux_amd64.deb",
-        "android": "https://github.com/KaringX/clashmi/releases/download/v1.0.27.1301/clashmi_1.0.27.1301_android_arm64-v8a.apk",
+        "version": "v1.0.28",
+        "windows": "https://github.com/KaringX/clashmi/releases/download/v1.0.28.1404/clashmi_1.0.28.1404_windows_x64.exe",
+        "mac": "https://github.com/KaringX/clashmi/releases/download/v1.0.28.1404/clashmi_1.0.28.1404_macos_universal.dmg",
+        "linux": "https://github.com/KaringX/clashmi/releases/download/v1.0.28.1404/clashmi_1.0.28.1404_linux_amd64.deb",
+        "android": "https://github.com/KaringX/clashmi/releases/download/v1.0.28.1404/clashmi_1.0.28.1404_android_arm64-v8a.apk",
         "github": "https://github.com/KaringX/clashmi",
         "ios": "https://apps.apple.com/us/app/clash-mi/id6744321968",
         "iosPrice": "Free"
